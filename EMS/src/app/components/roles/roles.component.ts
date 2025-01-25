ecf098b9a761ed714554f9817bae0a022b3a5225
owais khan <owaissalauddinkhan@gmail.com>
2025-01-26 00:54:12 +0530

    API intergration

--- EMS/src/app/components/roles/roles.component.ts
+++ EMS/src/app/components/roles/roles.component.ts
@@ -1,28 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component , inject, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../models/interfaces/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-    firstName: string ="owais";
-    lastName: string ="khan ";
-    dob:Date= new Date();
-    input: string= "button";
-    selectedstate: string = "";
+export class RolesComponent implements OnInit{
 
-    showsomemesaage (){
-    alert('Hello '+ this.firstName + ' ' + this.lastName + ' ' + this.dob);
-    }
-
-    showinput(){
-        alert('You clicked on ' + this.input);
-    }
-
-    showgratitude(message: string){
-        alert(message);
-    }
+   rolelist: IRole[]=[]
+   http= inject(HttpClient);
+   ngOnInit(){
+    this.getAllRoles();
+   }
+   getAllRoles(){
+    this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res:any)=>{
+        this.rolelist=res.data;
+    })
+   }
 }
+
